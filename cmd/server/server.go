9843a2fb0ba477893b87a5fecb3651db7527a209
Vitoriano <vitorianoernandes2012@gmail.com>
2022-06-22 21:03:28 -0300

    Implementando o client

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Vitoriano/go-gRPC/pb"
 	"github.com/Vitoriano/go-gRPC/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
